e211860e36f09fca04389354395fea579321c0ce
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-08-29 17:02:29 +0100

    added game options

--- script.js
+++ script.js
@@ -20,6 +20,25 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === 'Rock' && computerChoice === 'Paper') {
     console.log('You lose! Paper beats Rock');
     computerScore++;
+  } else if (humanChoice === 'Rock' && computerChoice === 'Scissors') {
+    console.log('You win! Rock beats Scissors');
+    humanScore++;
+  } else if (humanChoice === 'Rock' && computerChoice === 'Rock') {
+    console.log("It's a tie! Rock equals Rock");
+  } else if (humanChoice === 'Paper' && computerChoice === 'Rock') {
+    console.log('You win! Paper beats Rock');
+    humanScore++;
+  } else if (humanChoice === 'Paper' && computerChoice === 'Scissors') {
+    console.log('You lose! Scissors beats Paper');
+    computerScore++;
+  } else if (humanChoice === 'Scissors' && computerChoice === 'Rock') {
+    console.log('You lose! Rock beats Scissors');
+    computerScore++;
+  } else if (humanChoice === 'Scissors' && computerChoice === 'Paper') {
+    console.log('You win! Scissors beats Paper');
+    humanScore++;
+  } else if (humanChoice === 'Scissors' && computerChoice === 'Scissors') {
+    console.log("It's a tie! Scissors equals Scissors");
   }
 }
 
