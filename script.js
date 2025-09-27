2e7de7968dfa7ed6d55d3fc04a870c6beff11bb5
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-09-27 17:43:15 +0100

    added results div and removed console logs

--- script.js
+++ script.js
@@ -4,66 +4,85 @@ const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
 
+const resultsDiv = document.getElementById('results');
+
 function getComputerChoice() {
   const choice = ['Rock', 'Paper', 'Scissors'];
   const randomChoice = Math.floor(Math.random() * choice.length);
   return choice[randomChoice];
 }
 
+function displayResults(
+  humanSelection,
+  computerSelection,
+  roundResult,
+  humanScore,
+  computerScore
+) {
+  resultsDiv.innerHTML = `
+    <p>You chose: ${humanSelection}</p>
+    <p>Computer chose: ${computerSelection}</p>
+    <p>${roundResult}</p>
+    <p>Your Score: ${humanScore}</p>
+    <p>Computer Score: ${computerScore}</p>
+  `;
+}
+
 function playRound(humanChoice, computerChoice) {
+  let roundResult = '';
   if (humanChoice === 'Rock' && computerChoice === 'Paper') {
-    console.log('You lose! Paper beats Rock');
+    roundResult = 'You lose! Paper beats Rock';
     computerScore++;
   } else if (humanChoice === 'Rock' && computerChoice === 'Scissors') {
-    console.log('You win! Rock beats Scissors');
+    roundResult = 'You win! Rock beats Scissors';
     humanScore++;
   } else if (humanChoice === 'Rock' && computerChoice === 'Rock') {
-    console.log("It's a tie! Rock equals Rock");
+    roundResult = "It's a tie! Rock equals Rock";
   } else if (humanChoice === 'Paper' && computerChoice === 'Rock') {
-    console.log('You win! Paper beats Rock');
+    roundResult = 'You win! Paper beats Rock';
     humanScore++;
   } else if (humanChoice === 'Paper' && computerChoice === 'Scissors') {
-    console.log('You lose! Scissors beats Paper');
+    roundResult = 'You lose! Scissors beats Paper';
     computerScore++;
   } else if (humanChoice === 'Scissors' && computerChoice === 'Rock') {
-    console.log('You lose! Rock beats Scissors');
+    roundResult = 'You lose! Rock beats Scissors';
     computerScore++;
   } else if (humanChoice === 'Scissors' && computerChoice === 'Paper') {
-    console.log('You win! Scissors beats Paper');
+    roundResult = 'You win! Scissors beats Paper';
     humanScore++;
   } else if (humanChoice === 'Scissors' && computerChoice === 'Scissors') {
-    console.log("It's a tie! Scissors equals Scissors");
+    roundResult = "It's a tie! Scissors equals Scissors";
   } else if (humanChoice === 'Paper' && computerChoice === 'Paper') {
-    console.log("It's a tie! Paper equals Paper");
+    roundResult = "It's a tie! Paper equals Paper";
   }
+  displayResults(
+    humanChoice,
+    computerChoice,
+    roundResult,
+    humanScore,
+    computerScore
+  );
 }
 
 rockBtn.addEventListener('click', () => {
   const humanSelection = 'Rock';
   const computerSelection = getComputerChoice();
-  console.log(`You chose: ${humanSelection}`);
-  console.log(`Computer chose: ${computerSelection}`);
   playRound(humanSelection, computerSelection);
-  console.log(`Your Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
 });
 
 paperBtn.addEventListener('click', () => {
   const humanSelection = 'Paper';
   const computerSelection = getComputerChoice();
-  console.log(`You chose: ${humanSelection}`);
-  console.log(`Computer chose: ${computerSelection}`);
   playRound(humanSelection, computerSelection);
-  console.log(`Your Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
 });
 
 scissorsBtn.addEventListener('click', () => {
   const humanSelection = 'Scissors';
   const computerSelection = getComputerChoice();
-  console.log(`You chose: ${humanSelection}`);
-  console.log(`Computer chose: ${computerSelection}`);
   playRound(humanSelection, computerSelection);
-  console.log(`Your Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
+});
+scissorsBtn.addEventListener('click', () => {
+  const humanSelection = 'Scissors';
+  const computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
 });
